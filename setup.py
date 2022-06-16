c338ae64fb058f8c762b3fbcbda5dbf9d6986a65
Andrew <chan.2019@alumni.ubc.ca>
2022-06-15 18:10:04 -0700

    one more time and I'm going

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PSCN-Over-TheRainbow",
+    name="example-package-YOUR-USERNAME-HERE",
     version="0.0.1",
-    author="Over-TheRainbow",
-    description="A small PSCN package",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Over-TheRainbow/PSCN",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
